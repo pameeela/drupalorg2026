96f815692a29e230c8cba3eeaf5a0fd8e13a7388
Pamela <pnbarone@gmail.com>
2026-08-25 14:08:27 +1000

    Commit local settings and services for easier setup

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -1,4 +1,8 @@
 <?php
 
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
 // Use development service parameters.
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.local.yml';
